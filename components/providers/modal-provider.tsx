fb45955967a18aaf5f87a7b8c5cb23c2b73ee366
Yusuf Kahya <niqhtswordxd.yk@gmail.com>
2024-06-28 13:08:56 +0000

    feat: Add support for editing server details in modal

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,7 +2,8 @@
 
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { useEffect, useState } from "react";
-import { InviteModal } from "@/components//modals/invite-modal";
+import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -18,6 +19,7 @@ export const ModalProvider = () => {
     <>
       <CreateServerModal />
       <InviteModal />
+      <EditServerModal />
     </>
   );
 };
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -54,7 +54,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className="cursor-pointer px-3 py-2 text-sm">
+          <DropdownMenuItem
+            onClick={() => onOpen("editServer", { server })}
+            className="cursor-pointer px-3 py-2 text-sm"
+          >
             Server Settings
             <Settings className="ml-auto h-4 w-4" />
           </DropdownMenuItem>
